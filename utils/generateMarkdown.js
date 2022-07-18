1a607aa3800a17ffd46429639607338756493bd9
Shaitan5723 <105050107+Shaitan5723@users.noreply.github.com>
2022-07-18 15:40:10 +1000

    more testing for markup

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -16,13 +16,13 @@ function generateMarkdown(data) {
 ## Description
   ${data.description}
 ## Table of Contents
-    [Description](#description)
-    [Installation Instructions](#installation)
-    [Usage Information](#usage)
-    [License](#license)
-    [Contribution Guidelines](#contributing) 
-    [Tests](#tests)
-    [Questions](#questions)
+  1. [Description](#description)
+  2. [Installation Instructions](#installation)
+  3. [Usage Information](#usage)
+  4. [License](#license)
+  5. [Contribution Guidelines](#contributing) 
+  6. [Tests](#tests)
+  7. [Questions](#questions)
 ## Installation
   ${data.installation}
 ## Usage
@@ -33,7 +33,8 @@ function generateMarkdown(data) {
 ## Tests
   ${data.tests}
 ## Questions
-  Feel free to check out my other projects at [GitHub - ${data.username}](github.com/${data.username})
+  Feel free to check out my other projects at [GitHub - ${data.username}](github.com/${data.username}).
+
   For any additional questions, please feel free to reach out to me at <${data.email}>
 `;
 }
